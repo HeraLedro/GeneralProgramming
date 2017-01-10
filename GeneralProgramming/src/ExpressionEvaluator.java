470c248a2442d4973caad057248702a55f8b0361
Jonathan <jon_mcdevitt@hotmail.com>
2017-01-10 13:52:05 -0400

    Basic modifications
    Some basic modifications to clean the file and start everything new

---
+++ GeneralProgramming/src/ExpressionEvaluator.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Owner on 1/10/2017.
+ */
+
+import java.util.Scanner;
+
+public class ExpressionEvaluator {
+    public static void main(String[] args) {
+        /** Enter expression here   */
+        Scanner in = new Scanner(System.in);
+        String expression = in.nextLine();
+
+        String type = parseExpression(expression);
+    }
+
+    public static String parseExpression(String expression) {
+        String type = "";
+
+        return type;
+    }
+}
